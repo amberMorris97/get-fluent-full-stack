f94f096a716cc34e1e325a85c8ae645cbed2fc3e
Amber Morris <ambermorris1997@gmail.com>
2026-09-18 15:20:39 -0400

    prevent generate phrase from calculating if there is no backend

--- frontend/src/utils/generatePhrase.js
+++ frontend/src/utils/generatePhrase.js
@@ -1,4 +1,6 @@
 const generatePhrase = (phrases, callback) => {
+    if (!Array.isArray(phrases) || phrases.length === 0) return null;
+    
     let randomIndex = Math.floor(Math.random() * phrases.length);
     
     if (callback) callback(phrases[randomIndex].flashcardId);
